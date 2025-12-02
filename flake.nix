46576a68ae434323ce4bb10e30462f87aaea816d
Allar Klein <allarklein@gmail.com>
2025-12-02 05:49:09 +0200

    Add initial flake configuration and environment setup

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "AOC development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python313
+            python313Packages.flask
+            python313Packages.pip
+            python313Packages.numpy
+          ];
+
+          shellHook = ''
+            echo "AOC development environment loaded"
+          '';
+        };
+      }
+    );
+}
